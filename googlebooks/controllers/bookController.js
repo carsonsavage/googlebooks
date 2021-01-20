194f8ea1de52de82001791934803cbc349d4cd3a
Carson Savage <carsonesavage@gmail.com>
2021-01-20 13:14:59 -0600

    routes and models

---
+++ googlebooks/controllers/bookController.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+    db.Book
+        .find({})
+        .then(dbBooks => res.json(dbBooks))
+        .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+    db.Book
+        .create(req.body)
+        .then(dbBook => res.json(dbBook))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+    db.Book
+        .findByIdAndDelete(req.params.id)
+        .then(dbBook => res.json(dbBook))
+        .catch(err => res.status(422).json(err));
+    }
+};
